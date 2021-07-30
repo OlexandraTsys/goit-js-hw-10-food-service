b52bded396e3f37e90ce2c39e286c14cc36f201c
Al Tsys <olexandra.holubtsova@gmail.com>
2021-07-30 23:24:21 +0300

    внесены изменения в код

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import { switchTheme } from './themeSwitch/themeSwitch';
+import { createMenu } from './createMenu/createMenu';
+
+import '../sass/main.scss';
+
+// Theme switch
+switchTheme();
+
+// Markup
+createMenu();
